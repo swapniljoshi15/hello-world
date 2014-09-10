c5b17ec9bdedf8fed24c48b155006cfbe8feeae4
swapniljoshi15 <swapnil.joshi1590@gmail.com>
2014-09-09 18:05:22 -0700

    changes done in build.gradle and HelloConfig.scala file
    changes done in build.gradle and HelloConfig.scala file

--- src/main/scala/hello/HelloConfig.scala
+++ src/main/scala/hello/HelloConfig.scala
@@ -1,8 +1,13 @@
 package hello
 
+
+import org.springframework.boot._
+import org.springframework.boot.autoconfigure._
+import org.springframework.stereotype._
+import org.springframework.web.bind.annotation._
 import org.springframework.context.annotation.Configuration
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration
 import org.springframework.context.annotation.ComponentScan
+
 
 /**
  * This config class will trigger Spring @annotation scanning and auto configure Spring context.
@@ -10,15 +15,16 @@ import org.springframework.context.annotation.ComponentScan
  * @author saung
  * @since 1.0
  */
- 
+
 @Controller
 @Configuration
 @EnableAutoConfiguration
 @ComponentScan
 class HelloConfig {
 
-	@RequestMapping(Array("/"))
-	@ResponseBody
-	def home(): String = "Hello World!"
+        @RequestMapping(Array("/"))
+        @ResponseBody
+        def home(): String = "Hello World!"
 
 }
+
